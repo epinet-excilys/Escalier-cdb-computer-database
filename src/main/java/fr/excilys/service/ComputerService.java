dfdcf4a400616031eeb3bd81d3a58090ebf01187
epinet <epinet@excilys.com>
2020-03-06 18:43:05 +0100

    WIP COMPUTERSERVICE

--- src/main/java/fr/excilys/service/ComputerService.java
+++ src/main/java/fr/excilys/service/ComputerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import fr.excilys.dao.ComputerDAO;
+import fr.excilys.exception.DatabaseDAOException;
 import fr.excilys.model.Computer;
 
 public final class ComputerService {
@@ -13,7 +14,6 @@ public final class ComputerService {
 	private static volatile ComputerService instance = null;
 
 	private ComputerService() {
-		super();
 	}
 
 	public final static ComputerService getInstance() {
@@ -28,26 +28,41 @@ public final class ComputerService {
 	}
 
 	public void update(Computer computer) {
+		try {
 		computerDAO.update(computer);
+		}catch(DatabaseDAOException databaseDAOException) {
+			//TODO GERER LE RENVOIE UTILISATEUR
+		}
 	}
 
 	public void add(Computer computer) {
+		try {
 		computerDAO.create(computer);
+		}catch(DatabaseDAOException databaseDAOException) {
+			//TODO GERER LE RENVOIE UTILISATEUR
+		}
 	}
 
 	public void delete(int iD) {
+		try {
 		computerDAO.delete(iD);
+		}catch(DatabaseDAOException databaseDAOException) {
+			//TODO GERER LE RENVOIE UTILISATEUR
+		}
 	}
 
 	public Optional<Computer> findByID(int ID) {
 		Optional<Computer> computer = Optional.empty();
+		
 		computer = computerDAO.findByID(ID);
+		//TODO GERER LE RENVOIE UTILISATEUR
 		return computer;
 	}
 
 	public List<Computer> getAllComput() {
 		List<Computer> listComputer = new ArrayList<>();
 		listComputer = computerDAO.findAll();
+		//TODO GERER LE RENVOIE UTILISATEUR
 		return listComputer;
 
 	}
@@ -55,6 +70,7 @@ public final class ComputerService {
 	public List<Computer> getAllPaginateComput(int ligneDebutOffSet, int taillePage) {
 		List<Computer> listComputer = new ArrayList<>();
 		listComputer = computerDAO.findAllPaginate(ligneDebutOffSet, taillePage);
+		//TODO GERER LE RENVOIE UTILISATEUR
 		return listComputer;
 
 	}
@@ -62,20 +78,24 @@ public final class ComputerService {
 	public List<Computer> findAllPaginateSearchLike(String search, int ligneDebutOffSet, int taillePage) {
 		List<Computer> listComputer = new ArrayList<>();
 		listComputer = computerDAO.findAllPaginateSearchLike(search,ligneDebutOffSet, taillePage);
+		//TODO GERER LE RENVOIE UTILISATEUR
 		return listComputer;
 	}
 	
 	public List<Computer> findAllPaginateAlphabeticOrder(int ligneDebutOffSet, int taillePage) {
 		List<Computer> listComputer = new ArrayList<>();
 		listComputer = computerDAO.findAllPaginateAlphabeticOrder(ligneDebutOffSet, taillePage);
+		//TODO GERER LE RENVOIE UTILISATEUR
 		return listComputer;
 	}
 
 	public int getNbRows() {
+		//TODO
 		return computerDAO.getNbRow();
 	}
 	
 	public int getNbRowsSearch(String search) {
+		//TODO
 		return computerDAO.getNbRowSearch(search);
 	}
 
