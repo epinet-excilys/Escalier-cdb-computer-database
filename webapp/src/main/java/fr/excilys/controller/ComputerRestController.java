a86b814d0a8d22db5b4f209ee234c94b66d96e9a
epinet <epinet@excilys.com>
2020-06-29 14:12:11 +0200

    Sysout found

--- webapp/src/main/java/fr/excilys/controller/ComputerRestController.java
+++ webapp/src/main/java/fr/excilys/controller/ComputerRestController.java
@@ -73,8 +73,6 @@ public class ComputerRestController {
 	@PostMapping
 	@Transactional
 	public void createComputer(@RequestBody ComputerDTO computerDTO) throws DatabaseDAOException {
-
-		System.out.println(computerDTO);
 		Computer computerToCreate = computerMapper.fromComputerDTOToComputer(computerDTO);
 		computerService.add(computerToCreate);
 
